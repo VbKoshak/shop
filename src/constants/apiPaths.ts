4b66a75e159809561be1928388aaef9015db6c2f
vladboltrukanis <boltrukanisvlad@gmail.com>
2021-09-22 22:51:54 +0300

    task5: initial commit

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://5d09qqbry8.execute-api.us-east-2.amazonaws.com/dev',
   order: 'https://5d09qqbry8.execute-api.us-east-2.amazonaws.com/dev',
-  import: 'https://5d09qqbry8.execute-api.us-east-2.amazonaws.com/dev',
+  import: 'https://pgj4h6q3nc.execute-api.us-east-2.amazonaws.com/dev',
   bff: 'https://5d09qqbry8.execute-api.us-east-2.amazonaws.com/dev',
   cart: 'https://5d09qqbry8.execute-api.us-east-2.amazonaws.com/dev',
 };
